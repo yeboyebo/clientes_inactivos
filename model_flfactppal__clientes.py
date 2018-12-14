90799e56f67782527950114f62ea3907934ecd6d
andres.yeboyebo@gmail.com <andres@yeboyebo.es>
2018-12-14 11:55:46 +0100

    Arreglos para extender de flfacturac

--- model_flfactppal__clientes.py
+++ model_flfactppal__clientes.py
@@ -1,47 +1,14 @@
-# @class_declaration interna_clientes #
-import importlib
-
-from YBUTILS.viewREST import helpers
-
-from models.flfactppal import models as modelos
-
-
-class interna_clientes(modelos.mtd_clientes, helpers.MixinConAcciones):
-    pass
-
-    class Meta:
-        proxy = True
-
 
 # @class_declaration clientes_inactivos_clientes #
-class clientes_inactivos_clientes(interna_clientes, helpers.MixinConAcciones):
+class clientes_inactivos_clientes(flfactppal_clientes, helpers.MixinConAcciones):
     pass
 
     class Meta:
         proxy = True
-
-    def initValidation(name, data=None):
-        return form.iface.initValidation(name, data)
-
-    def iniciaValoresLabel(self, template=None, cursor=None, data=None):
-        return form.iface.iniciaValoresLabel(self, template, cursor)
-
-    def bChLabel(fN=None, cursor=None):
-        return form.iface.bChLabel(fN, cursor)
-
-    def getFilters(self, name, template=None):
-        return form.iface.getFilters(self, name, template)
-
-    def getForeignFields(self, template=None):
-        return form.iface.getForeignFields(self, template)
-
-    def getDesc():
-        return form.iface.getDesc()
 
     @helpers.decoradores.accion(aqparam=["oParam"])
     def queryGrid_clientesInactivos(model, filters):
-        print("_________", filters)
-        return form.iface.queryGrid_clientesInactivos(model)
+        return form.iface.queryGrid_clientesInactivos(model, filters)
 
     @helpers.decoradores.accion(aqparam=["oParam"])
     def queryGrid_ventasClientes(model):
@@ -55,17 +22,3 @@ class clientes_inactivos_clientes(interna_clientes, helpers.MixinConAcciones):
     def dameEmails(self, oParam):
         return form.iface.dameEmails(self, oParam)
 
-
-# @class_declaration clientes #
-class clientes(clientes_inactivos_clientes, helpers.MixinConAcciones):
-    pass
-
-    class Meta:
-        proxy = True
-
-
-definitions = importlib.import_module("models.flfactppal.clientes_def")
-form = definitions.FormInternalObj()
-form._class_init()
-form.iface.ctx = form.iface
-form.iface.iface = form.iface
--- model_flfactppal__clientes_def.py
+++ model_flfactppal__clientes_def.py
@@ -1,50 +1,21 @@
-# @class_declaration interna #
-from YBLEGACY import qsatype
-
-
-class interna(qsatype.objetoBase):
-
-    ctx = qsatype.Object()
-
-    def __init__(self, context=None):
-        self.ctx = context
-
 
 # @class_declaration clientes_inactivos #
 from YBLEGACY.constantes import *
 
 
-class clientes_inactivos(interna):
+class clientes_inactivos(flfactppal):
 
-    def clientes_inactivos_initValidation(self, name, data=None):
-        response = True
-        return response
-
-    def clientes_inactivos_iniciaValoresLabel(self, model=None, template=None, cursor=None):
-        labels = {}
-        return labels
-
-    def clientes_inactivos_bChLabel(self, fN=None, cursor=None):
-        labels = {}
-        return labels
-
-    def clientes_inactivos_getFilters(self, model, name, template=None):
-        filters = []
-        return filters
-
-    def clientes_inactivos_getForeignFields(self, model, template=None):
-        fields = []
-        return fields
-
-    def clientes_inactivos_getDesc(self):
-        desc = None
-        return desc
-
-    def clientes_inactivos_queryGrid_clientesInactivos(self, model):
+    def clientes_inactivos_queryGrid_clientesInactivos(self, model, filters):
+        fecha = ''
+        if(filters):
+            print(filters['[d_fecha]'])
+            fecha = "'" + filters['[d_fecha]'] + "'"
+        if not fecha or fecha == "''":
+            fecha = 'CURRENT_DATE'
         query = {}
         query["tablesList"] = ("clientes,pedidoscli")
         query["select"] = ("c.codcliente, c.nombre, c.email, c.telefono1, MAX(antes.fecha) as fecha, d.direccion")
-        query["from"] = ("clientes AS c INNER JOIN pedidoscli AS antes ON c.codcliente = antes.codcliente AND antes.fecha < CURRENT_DATE LEFT OUTER JOIN pedidoscli AS despues ON c.codcliente = despues.codcliente AND despues.fecha >= CURRENT_DATE INNER JOIN dirclientes d ON c.codcliente = d.codcliente and d.domfacturacion is true")
+        query["from"] = ("clientes AS c INNER JOIN pedidoscli AS antes ON c.codcliente = antes.codcliente AND antes.fecha < " + fecha + " LEFT OUTER JOIN pedidoscli AS despues ON c.codcliente = despues.codcliente AND despues.fecha >= " + fecha + " INNER JOIN dirclientes d ON c.codcliente = d.codcliente and d.domfacturacion is true")
         query["where"] = "antes.codcliente IS NOT NULL and despues.codcliente IS NULL"
         query["groupby"] = " c.codcliente, c.nombre, c.email, c.telefono1, d.direccion"
         query["orderby"] = "c.nombre DESC"
@@ -88,26 +59,8 @@ class clientes_inactivos(interna):
     def __init__(self, context=None):
         super(clientes_inactivos, self).__init__(context)
 
-    def initValidation(self, name, data=None):
-        return self.ctx.clientes_inactivos_initValidation(name, data)
-
-    def iniciaValoresLabel(self, model=None, template=None, cursor=None):
-        return self.ctx.clientes_inactivos_iniciaValoresLabel(model, template, cursor)
-
-    def bChLabel(self, fN=None, cursor=None):
-        return self.ctx.clientes_inactivos_bChLabel(fN, cursor)
-
-    def getFilters(self, model, name, template=None):
-        return self.ctx.clientes_inactivos_getFilters(model, name, template)
-
-    def getForeignFields(self, model, template=None):
-        return self.ctx.clientes_inactivos_getForeignFields(model, template)
-
-    def getDesc(self):
-        return self.ctx.clientes_inactivos_getDesc()
-
-    def queryGrid_clientesInactivos(self, model):
-        return self.ctx.clientes_inactivos_queryGrid_clientesInactivos(model)
+    def queryGrid_clientesInactivos(self, model, filters):
+        return self.ctx.clientes_inactivos_queryGrid_clientesInactivos(model, filters)
 
     def queryGrid_ventasClientes(self, model):
         return self.ctx.clientes_inactivos_queryGrid_ventasClientes(model)
@@ -118,22 +71,3 @@ class clientes_inactivos(interna):
     def dameEmails(self, model, oParam):
         return self.ctx.clientes_inactivos_dameEmails(model, oParam)
 
-
-# @class_declaration head #
-class head(clientes_inactivos):
-
-    def __init__(self, context=None):
-        super(head, self).__init__(context)
-
-
-# @class_declaration ifaceCtx #
-class ifaceCtx(head):
-
-    def __init__(self, context=None):
-        super(ifaceCtx, self).__init__(context)
-
-
-# @class_declaration FormInternalObj #
-class FormInternalObj(qsatype.FormDBWidget):
-    def _class_init(self):
-        self.iface = ifaceCtx(self)
